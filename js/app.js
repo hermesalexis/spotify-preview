1dbf62343642bfb1f926d1b4de3237fe45860529
hermesalexis <hermesagm@gmail.com>
2017-02-23 18:31:42 -0500

    retrive info tracks of Spotify API

--- js/app.js
+++ js/app.js
@@ -1,35 +1,61 @@
 
-var inputTextSearch= "";
-var arrayTracks = [];
-var customInfoTrack = {artist:"", trackName:"", urlImageAlbum:"", urlTrackPreview:""};
+function setCustomInfoTrack(item){
+  var customInfoTrack = {artist:"", trackName:"", urlImageAlbum:"", urlTrackPreview:""};
 
+  customInfoTrack.artist = item.artists[0].name;
+  customInfoTrack.trackName = item.name;
+  customInfoTrack.urlImageAlbum = item.album.images[0].url;
+  customInfoTrack.urlTrackPreview = item.preview_url;
 
+  return customInfoTrack;
+}
+
+function selectInfoTracks(infoTracks){
+  var arrayCustomInfoTracks = [];
+
+  $.each(infoTracks, function(key, tracks){
+
+    $.each(tracks.items, function(index, item){
+         arrayCustomInfoTracks.push(setCustomInfoTrack(item));
+    });
+
+  });
+
+  return arrayCustomInfoTracks;
+}
+
+function getInputTextSearch (inputSearch){//ajusta espacios con "+" como lo pide el API
+   return inputSearch.val().split(" ").join("+");
+}
+
+//////////////////////////// events
 
 $("#search-track").keypress(function(e){
    if(e.keyCode == 13){
+
       e.preventDefault();
-       
-       inputTextSearch = $("#search-track").val().split(" ").join("+");
+
+      var inputTextSearch = getInputTextSearch($("#search-track")); 
 
       $.ajax({
         url: 'https://api.spotify.com/v1/search?q=' + inputTextSearch + '&type=track',
         dataType: 'json'
       })
-      .done(function(infoTrack) {
-        $.each(infoTrack, function(key, tracks){
-          $.each(tracks.items, function(index, item){
-               customInfoTrack.artist = item.artists[0].name;
-               customInfoTrack.trackName = item.name;
-               customInfoTrack.urlImageAlbum = item.album.images[0].url;
-               customInfoTrack.urlTrackPreview = item.preview_url;
-               arrayTracks.push(customInfoTrack);
-          });
+      .done(function(jsonTracks) {
+        var resultSearch = selectInfoTracks(jsonTracks);
+        $.each(resultSearch, function(index, track){
+          $(".wrapper").append('<div>' +
+                                  '<ol>'+
+                                       '<li>' + track.artist + '</li>'+
+                                       '<li>' + track.trackName + '</li>'+
+                                       '<li>' + '<img src="' + track.urlImageAlbum + '">' + '</li>' +
+                                       '<li>' + '<a href="' + track.urlTrackPreview + '"> Track Preview </a> </li>'+
+                                  '</ol>' +
+                                '</div>');
         });
-        console.log(arrayTracks);
       });
 
     }
 
 });
-
 
