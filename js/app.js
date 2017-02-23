941535a0ca0b848b460ae2575a15cfd8128cb64e
hermesalexis <hermesagm@gmail.com>
2017-02-23 12:35:26 -0500

    extrae la informacion del json spotify

--- js/app.js
+++ js/app.js
@@ -1,21 +1,35 @@
 
 var inputTextSearch= "";
+var arrayTracks = [];
+var customInfoTrack = {artist:"", trackName:"", urlImageAlbum:"", urlTrackPreview:""};
 
-$("#search-track").keydown(function(e){
+
+
+$("#search-track").keypress(function(e){
    if(e.keyCode == 13){
+      e.preventDefault();
+       
        inputTextSearch = $("#search-track").val().split(" ").join("+");
 
-       $.ajax({
-         //url: 'https://api.spotify.com/v1/search?q=' + inputTextSearch + '&type=track',
-         url:'https://api.spotify.com/v1/search?q=muse&type=track',
-         dataType: 'json'
-       })
-       .done(function(infoMusic) {
-         console.log(infoMusic);
-       });
+      $.ajax({
+        url: 'https://api.spotify.com/v1/search?q=' + inputTextSearch + '&type=track',
+        dataType: 'json'
+      })
+      .done(function(infoTrack) {
+        $.each(infoTrack, function(key, tracks){
+          $.each(tracks.items, function(index, item){
+               customInfoTrack.artist = item.artists[0].name;
+               customInfoTrack.trackName = item.name;
+               customInfoTrack.urlImageAlbum = item.album.images[0].url;
+               customInfoTrack.urlTrackPreview = item.preview_url;
+               arrayTracks.push(customInfoTrack);
+          });
+        });
+        console.log(arrayTracks);
+      });
 
-       console.log("salio AJAX");
-   }
+    }
+
 });
 
 
